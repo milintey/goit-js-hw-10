6a1207301e61127a11788d0f686fffa17ce62b44
Ilya Milinteev <ilyamilint@gmail.com>
2022-06-30 17:54:50 +0300

    comit 2

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+import Notiflix from 'notiflix';
+
+export function fetchCountries(nameCountry) {
+return fetch(`https://restcountries.com/v3.1/name/${nameCountry}?fields=name,capital,population,languages,flags`)
+        .then(response => {
+            if (response.status === 500) {
+                Notiflix.Notify.failure("Oops, there is no country with that name");
+            }
+
+            return response.json();
+        }).catch(error => console.log(error));
+
+};
